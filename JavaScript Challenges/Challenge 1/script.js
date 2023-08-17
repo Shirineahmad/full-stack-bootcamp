b94363fb46ae684188c799e72c494b6a89d4c6b0
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-17 22:39:46 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,1 +1,14 @@
 console.log('I am linked now!');
+// step 2 
+var names = "shirine";
+console.log(alert(names));
+// step3
+var age = 27;
+let lastName = "ahmad";
+const DOB = "1996 / 3 / 4";
+age = 28;
+lastName = "Ahmad";
+DOB = "1996 / 4 / 3";
+console.log(lastName, age, DOB);
+
+
