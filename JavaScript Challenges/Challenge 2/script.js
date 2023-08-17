14541cfc17c3d37b3e9990c52f094f2584a6d64f
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-17 23:22:43 +0300

    Challenge 2 - Done with step 2

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -24,3 +24,20 @@ console.log(expression1);
 console.log(expression2);
 console.log(expression3);
 console.log(expression4);
+// step2
+const age = 25;
+const hasDriverLicense = true;
+const hasCar = false;
+
+var canDrive = false;
+if (age >= 18 && hasDriverLicense == true) {
+    canDrive = true;
+}
+var canRentCar = false;
+if (age >= 22 && hasDriverLicense == true) {
+    canRentCar = true;
+}
+var needDriver = false;
+if (age < 22 && hasDriverLicense == false && hasCar==false ) {
+    needDriver = true;
+}
