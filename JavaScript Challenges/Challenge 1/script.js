a71949841239206608a68e72f282319446b38e80
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-17 22:40:06 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -11,4 +11,15 @@ lastName = "Ahmad";
 DOB = "1996 / 4 / 3";
 console.log(lastName, age, DOB);
 
-
+//  step4
+console.log("This is a text :", typeof ("This is a text"), "true:", typeof (true),
+    "false: ", typeof (false), "1215", typeof (1215), "999", typeof ("999"), "{}:", typeof ({}), "[]: ", typeof ([]), "null:", typeof (null), "undefined:", typeof (undefined));
+var num = 123;
+var character = [];
+let str = "245";
+let bool = true;
+let none = null;
+let defined = undefined;
+console.log("num:", typeof (num), "character:", typeof (character),
+    "str:", typeof (str), "bool", typeof (bool), "none", typeof (none), "defined", typeof (defined));
+    
