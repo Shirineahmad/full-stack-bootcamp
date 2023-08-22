53395b854c15b6fdd1f3653f8ac5ff5d97d12228
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-22 14:57:14 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,11 @@
+// Challenge 4
+
+// Step 1: Arrays and Objects
+// Array Creation
+var fiveNumbers = [2, 4, 5, 6, 8];
+// Object Creation
+var name = shirine;
+var age = 27;
+var height = 156;
+var nationality = Syria;
+var personalInfo = { name, age, height, location, nationality };
