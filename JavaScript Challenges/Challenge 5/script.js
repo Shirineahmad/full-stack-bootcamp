cf2e3ab486dbd5d9505a5f9abdcbb836abe1b080
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-23 21:57:24 +0300

    Challenge 5 - Done with step 2

--- JavaScript Challenges/Challenge 5/script.js
+++ JavaScript Challenges/Challenge 5/script.js
@@ -32,3 +32,49 @@ p.style.fontWeight = "helvetica";
 p.style.border = "1px solide black";
 let v = document.querySelector("h1");
 v.style.fontStyle = "italic";
+// Step 2: Event Handling
+// Multiple Event Listeners
+let button = document.createElement("button");
+button.innerText = "follow";
+document.body.appendChild(button);
+button.addEventListener("mouseover",  ()=> {
+    button.style.backgroundColor = "orange";
+})
+button.addEventListener("mouseout", ()=> {
+    button.style.backgroundColor = "red";
+})
+// Event Delegation
+let div1 = document.getElementById("original");
+
+    let div2 = document.querySelector(".container");
+
+    let buttons = document.querySelectorAll("#buttonContainer");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            console.log("Clicked button text:", button.textContent);
+        });
+    });
+// Form Data Extraction
+let formData = document.createElement("form");
+let inputName = document.createElement("input");
+inputName.type = "text";
+let inputEmail = document.createElement("input");
+inputName.type = "email";
+let buttonForm = document.createElement("button");
+buttonForm.type = "submit";
+buttonForm.textContent = "Submit";
+document.body.appendChild(formData);
+formData.appendChild(inputName);
+formData.appendChild(inputEmail);
+formData.appendChild(buttonForm);
+formData.style.display = "flex";
+formData.style.flexDirection = "column";
+ buttonForm.style.width ="100px";
+inputName.style.width = "300px";
+inputEmail.style.width = "300px";
+buttonForm.addEventListener("click", () => {
+    console.log(inputName.value,inputEmail.value);
+
+})
+
