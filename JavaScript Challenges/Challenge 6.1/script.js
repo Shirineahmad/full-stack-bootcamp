9bd5796b6fd5165ed1784c8be2e3ed6ed1e99a4c
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-29 10:42:26 +0300

    Challenge 6.1 - Done with Step 4

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -29,11 +29,28 @@ async function fetchData() {
     try {
         let response2 = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
         let data2 = await response2.json();
-        console.log(data2)
+        console.log(data2.text)
     } catch (error) {
         console.error("error ", error);
         
     }
 };
 fetchData();
-// 
+// step 4
+let array = [];
+async function fetchMultiple() {
+    try {
+        array.push ( await Promise.all([
+            fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json()),
+            fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json()),
+            fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json())
+        ]))
+
+        console.log(array);
+    } catch (error) {
+        console.log("Error fetching data:", error);
+    }
+}
+
+fetchMultiple();
+
