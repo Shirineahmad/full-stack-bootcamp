24b71f8dcc452fbd006ffd094995b9abfe75d825
Shirineahmad <shirine.ahmad@hotmail.com>
2023-08-29 10:15:45 +0300

    Challenge 6.1 - Done with Step 2

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -15,3 +15,12 @@ delay(10000)
     .then((message) => {
         console.log(message) 
     })
+    // Step 2: Fetch a Random Fact
+fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+    .then(response => response.json())
+    .then(response=> {
+        console.log(respons.text)
+    })
+    .catch(error => {
+        console.log("error",error);
+    });
